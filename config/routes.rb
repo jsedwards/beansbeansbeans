8c042b613b7d539de44bd15624fff728a7200f13
Jeff Edwards <js_edwards@yahoo.com>
2014-03-11 10:34:41 -0700

    added user sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Beansbeansbeans::Application.routes.draw do
   root "listings#index"
   resources :listings
   resources :users
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
 end
