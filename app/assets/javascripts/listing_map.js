2482fe9b74b6ed80f3dfaca1c331298e55bae914
Jeff Edwards <js_edwards@yahoo.com>
2014-03-13 15:02:52 -0700

    added check for user location - if out of sf or no location, map centered on location

--- app/assets/javascripts/listing_map.js
+++ app/assets/javascripts/listing_map.js
@@ -8,13 +8,23 @@
       function onSuccess(position){
         var user_lat = position.coords.latitude;
         var user_lng = position.coords.longitude;
+        var point_lat = $('#map').data("lat")
+        var point_lng = $('#map').data("lon")
+        if(user_lat < 37.639830 || user_lat > 37.9298240 || user_lng < -123.173825 || user_lng > -122.281780){
+          console.log("not close to SF")
+          user_lat = point_lat;
+          user_lng = point_lng;
+        }
         var map = L.map('map').setView([user_lat,user_lng], 14);
           L.tileLayer('http://otile4.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="www.openstreetmap.org/copyright">OpenStreetMap</a>'
           }).addTo(map);
-       var point_lat = $('#map').data("lat")
-     var point_lng = $('#map').data("lon")
-    L.marker([point_lat, point_lng]).addTo(map)      
+     var std_icon = L.icon({
+      iconUrl: '/assets/images/marker-icon.png',
+      iconSize: [25,41],
+      iconAnchor: [12,41]
+     })
+    var marker = L.marker([point_lat, point_lng], {icon: std_icon}).addTo(map);      
    }
    function onError(){
    }
